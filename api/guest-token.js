597cac37a5f0246f10bfec2d3546549ebda61439
1rtapc9 <arantrydert@gmail.com>
2025-12-04 17:19:35 -0500

    Update guest-token.js

--- api/guest-token.js
+++ api/guest-token.js
@@ -1,3 +1,5 @@
+import fetch from 'node-fetch';
+
 export default async function handler(req, res) {
     const clientId = '9049e4208b1c4f81a80a3b03948b30e9';
     const clientSecret = 'fb9fd4b4bb044699965318faac6311b4';
@@ -5,11 +7,12 @@ export default async function handler(req, res) {
     const tokenRes = await fetch('https://accounts.spotify.com/api/token', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
             'Authorization': 'Basic ' + Buffer.from(`${clientId}:${clientSecret}`).toString('base64'),
+            'Content-Type': 'application/x-www-form-urlencoded'
         },
-        body: 'grant_type=client_credentials',
+        body: 'grant_type=client_credentials'
     });
+
     const data = await tokenRes.json();
     res.status(200).json(data);
 }
