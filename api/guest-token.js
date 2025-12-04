de48984ef8c88e3d21c7569f29893f8a97607533
1rtapc9 <arantrydert@gmail.com>
2025-12-04 17:12:50 -0500

    Create guest-token.js

---
+++ api/guest-token.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+    const clientId = '9049e4208b1c4f81a80a3b03948b30e9';
+    const clientSecret = 'fb9fd4b4bb044699965318faac6311b4';
+
+    const tokenRes = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + Buffer.from(`${clientId}:${clientSecret}`).toString('base64'),
+        },
+        body: 'grant_type=client_credentials',
+    });
+    const data = await tokenRes.json();
+    res.status(200).json(data);
+}
